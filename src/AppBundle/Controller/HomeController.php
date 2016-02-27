7f01b59fd0dd3201bc25aa11f0ff8a700e71c093
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-02-27 17:42:07 +0100

    Index controller and view

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -4,15 +4,38 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $media = $this->getDoctrine()->getRepository('AppBundle:Media')->findAll();
+        return $this->render('home/index.html.twig', array(
+            'media' => $media
+        ));
+    }
+
+    /**
+     * @Route("/catalog/{mediaType}/", name="catalog")
+     */
+    public function catalogAction($mediaType)
+    {
+        $media      = $this->getDoctrine()->getRepository('AppBundle:Media')->findByMediaType($mediaType);
+        $mediaTypes = $this->getDoctrine()->getRepository('AppBundle:MediaType')->findOneById($mediaType);
+
+        if(!$media){
+            $this->addFlash(
+                'notice',
+                'No existe nada aún, vuelve pasados unos días'
+            );
+        }
+
+        return $this->render('home/catalog.html.twig', array(
+            'media'      => $media,
+            'mediaTypes' => $mediaTypes
+        ));
     }
 }
