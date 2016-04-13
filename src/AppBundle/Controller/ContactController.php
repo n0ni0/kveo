eb3081b2e8ad9f0eadf714ee8e9ea4c7c3497db8
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-04-13 23:58:23 +0200

    Created contact form

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Form\ContactFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/contact/", name="contact_form")
+     */
+    public function showContactAction(Request $request)
+    {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isValid() && $form->isSubmitted()){
+            $data = $form->getData();
+            $this->get('kveo_mailer')->sendMail($data);
+
+            $request->getSession()->getFlashBag()->add(
+                'notice',
+                $this->get('translator')->trans('flash.contact', array(), 'messages'
+                ));
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('contact/contactForm.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
