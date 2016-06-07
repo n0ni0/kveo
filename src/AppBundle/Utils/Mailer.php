90ab03ea297236d5f4520d8cb381f2052601d247
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-06-07 16:39:22 +0200

    Replaced email and test names

--- src/AppBundle/Utils/Mailer.php
+++ src/AppBundle/Utils/Mailer.php
@@ -22,9 +22,9 @@ class Mailer
         $message = \Swift_Message::newInstance()
             ->setContentType('text/html')
             ->setSubject('Formulario de contacto')
-            ->setFrom(array('mecaigotolosdias@gmail.com' => 'noreply@kveo.local'))
+            ->setFrom(array('example.email@kveo.local' => 'noreply@kveo.local'))
             ->setTo($user->getEmail())
-            ->setBcc('ajimenez.bf@gmail.com')
+            ->setBcc('example.email@kveo.local')
             ->setBody($this->templating->render('contact/mailTemplate.html.twig',
                 array('name'    => $user->getUsername(),
                       'email'   => $user->getEmail(),
