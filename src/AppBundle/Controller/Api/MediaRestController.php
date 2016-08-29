d3bc7fa8b2d83fe054811530bca80e10796a0598
Antonio Jiménez <ajimenez.bf@gmail.com>
2016-08-29 19:55:58 +0200

    Added Api doc in mediaRestController

--- src/AppBundle/Controller/Api/MediaRestController.php
+++ src/AppBundle/Controller/Api/MediaRestController.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Media;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
 use FOS\RestBundle\Controller\Annotations\View;
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
  * @RouteResource("/media")
@@ -16,6 +17,20 @@ class MediaRestController extends FOSRestController
     /**
      * @return array
      * @View(serializerGroups={"list"})
+     * @ApiDoc(
+     *     description="Return media info by id with most important fields",
+     *     requirements={
+     *      {"name"="id", "dataType"="integer", "description"="media id"}
+     *     },
+     *     output={
+     *      "class"  = "AppBundle\Entity\Media",
+     *      "groups" = "list"
+     *     },
+     *     statusCodes={
+     *      200 = "Returned when successful",
+     *      404 = "Returned when not found"
+     *     }
+     * )
      */
     public function basicInfoAction(Media $id){
         return $this->getDoctrine()->getRepository('AppBundle:Media')->find($id);
@@ -24,6 +39,20 @@ class MediaRestController extends FOSRestController
     /**
      * @param Media $id
      * @return object
+     * @ApiDoc(
+     *     description="Return media info by id with all fields",
+     *     requirements={
+     *      {"name"="id", "dataType"="integer", "description"="media id"}
+     *     },
+     *     output={
+     *      "class"  = "AppBundle\Entity\Media",
+     *      "groups" = "details"
+     *     },
+     *     statusCodes={
+     *      200 = "Returned when successful",
+     *      404 = "Returned when not found"
+     *     }
+     * )
      */
     public function fullInfoAction(Media $id)
     {
